2dc7f011a964a708924253b8e1c8b7134b5a41be
Drew Neil <a.neil@pccl.co.uk>
2009-08-19 10:24:44 +0100

    Removing 'fat_free_issues' in preparation to add 'crm_issues'

--- vendor/plugins/fat_free_issues/app/models/account_issue.rb
+++
@@ -1,5 +0,0 @@
-class AccountIssue < ActiveRecord::Base
-  belongs_to :account
-  belongs_to :issue
-  validates_presence_of :account_id, :issue_id
-end
--- vendor/plugins/fat_free_issues/config/routes.rb
+++
@@ -1,3 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.resources :issues,      :has_many => :comments, :collection => { :search => :get, :auto_complete => :post, :options => :get, :redraw => :post }
-end
