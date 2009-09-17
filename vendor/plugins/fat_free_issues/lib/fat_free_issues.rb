6823cd0de8ba79a7e59b78a48aef4cdb3173ccb9
Drew Neil <a.neil@pccl.co.uk>
2009-09-17 15:20:18 +0100

    Removed fat_free_issues plugin, crm_issues still present.
    I thought I had already done this. Wierd. :-/

--- vendor/plugins/fat_free_issues/lib/fat_free_issues.rb
+++
@@ -1,9 +0,0 @@
-# fat_free_issues.rb
-
-# Extend :account model to add :issues association.
-Account.send(:include, AccountIssueAssociations)
-
-ActivityObserver.instance.send :add_observer!, Issue
-
-# Add :issues plugin helpers.
-ActionView::Base.send(:include, IssuesHelper)
